925aefcec1d91b6a7e6b4154345f33b78709e343
icoriha <56757293+icoriha@users.noreply.github.com>
2022-08-16 21:11:28 +0900

    可読性向上

--- lib/domain/i_api/i_github_api.dart
+++ lib/domain/i_api/i_github_api.dart
@@ -1,6 +1,8 @@
 import 'package:gethub/domain/model/github_repo.dart';
 
 abstract class IGitHubAPI {
-  Future<List<GitHubRepo>> searchRepos(String searchWord,
-      {required int targetPage});
+  Future<List<GitHubRepo>> searchRepos(
+    String searchWord, {
+    required int targetPage,
+  });
 }
