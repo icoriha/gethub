42495334b99bd93f692c2d5acd6de80129c67b2d
icoriha <56757293+icoriha@users.noreply.github.com>
2022-08-20 21:28:44 +0900

    設定画面の多言語対応

--- lib/ui/page/setting_page.dart
+++ lib/ui/page/setting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gethub/notifier/app_theme_notifier.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SettingPage extends ConsumerWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -9,17 +10,19 @@ class SettingPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final notifier = ref.watch(appThemeNotifierProvicer.notifier);
     final appTheme = ref.watch(appThemeNotifierProvicer);
+    final l10n = AppLocalizations.of(context)!;
+
     return Scaffold(
-      appBar: AppBar(title: Text('設定')),
+      appBar: AppBar(title: Text(l10n.settings)),
       body: ListView(
         children: [
           SwitchListTile(
-            title: Text('端末設定のモードを使用'),
+            title: Text(l10n.useDeviceMode),
             value: appTheme.isDevice,
             onChanged: (bool _) => notifier.toggleIsDevice(),
           ),
           SwitchListTile(
-            title: Text('ダークモード'),
+            title: Text(l10n.darkMode),
             value: appTheme.isDark,
             onChanged: (bool _) => notifier.toggleIsDark(),
           ),
